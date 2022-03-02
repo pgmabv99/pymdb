c8d9d03dcfc74903b6ed676c9e753d42dbf9735d
alexey vorovich <pgmabv99@gmail.com>
2022-03-02 16:58:22 -0500

    fix

--- test.py
+++ test.py
@@ -2,48 +2,47 @@
 
 import os,json
 import sys
+import threading
+
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(1, '/root/pyutil')
 
 
 from  utz import utz,ddict,uos
 
+
 class test:
     def __init__(self):
         utz.logset()
         utz.print("hhhhhhhhhhhhhhhhhhhh")
         utz.enter2()
+        self.uos1=uos()
 
-    def kpods(self):
+    def kpods(self, cls):
         utz.enter2()
-        uos1=uos()
+        
 
         cmd=" kubectl get namespaces -o json "
-        resp=uos1.uoscall(cmd)
+        resp=self.uos1.uoscall(cmd)
         # utz.print(resp[uos.BUFOUT])
 
         respj=json.loads(resp[uos.BUFOUT])
         utz.jprint(respj["items"][0]["kind"])
         return
-
         
 
-    def gcp_cluster(self):
+    def gcp_cluster(self, cls ):
         utz.enter2()
-        uos1=uos()
-
-        cls="cls1"
-
+         
         cmd=" gcloud container clusters delete {cls} -q  --format=json ".format(cls=cls)
-        resp=uos1.uoscall(cmd)
+        resp=self.uos1.uoscall(cmd)
         utz.print(resp[uos.BUFOUT])
 
         # respj=json.loads(resp[uos.BUFOUT])
         # utz.jprint(respj["currentNodeCount"])
 
-
         cmd=" gcloud container clusters create {cls} --format=json ".format(cls=cls)
-        resp=uos1.uoscall(cmd)
+        resp=self.uos1.uoscall(cmd)
         # utz.print(resp[uos.BUFOUT])
 
         respj=json.loads(resp[uos.BUFOUT])
@@ -52,18 +51,49 @@ class test:
         return
 
 
+      
+    def thr1(self,ithr):
+        utz.enter2()
 
+        cls="cls"+str(ithr)
 
+        # with self._lock:
+        utz.print("thread: begin work ithr={ithr}".format(ithr=ithr))
+        # nsec=2
+        # utz.sleep(nsec,"sleep ithr={ithr} ".format(ithr=ithr))
+        self.gcp_cluster(cls)
+        self.kpods(cls)
+        utz.print("thread: end   work ithr={ithr}".format(ithr=ithr))
+
+        return 
+
+    def thr_run(self):
+        utz.enter2()
+        self._lock = threading.Lock()
+
+        lst_thr=list()
+        for ithr in range(2):
+            # utz.sleep(1,"sleep ")
+            pthr=threading.Thread(target=self.thr1,args=(ithr,))
+            lst_thr.append(pthr)
+            pthr.start()
+
+        for pthr in lst_thr:
+            pthr.join()
+
+        return 
+
+def main():
+    test1=test()
+
+    test1.thr_run()
+    # cls="cls1"
+    # test1.gcp_cluster(cls)
+    # test1.kpods(cls)
 
 
 
     
-   
 
-
-
-test1=test()
-
-# test1.kpods()
-test1.gcp_cluster()
+main()
 

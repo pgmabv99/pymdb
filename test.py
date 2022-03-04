1a230a7a50623ca38596e291651578c67c7e82e4
alexey vorovich <pgmabv99@gmail.com>
2022-03-04 11:21:34 -0500

    +

--- test.py
+++ test.py
@@ -18,16 +18,30 @@ class test:
         utz.enter2()
         self.uos1=uos()
 
-    def kpods(self, cls):
+    def namespaces(self, cls):
         utz.enter2()
-        
-
+       
         cmd=" kubectl get namespaces -o json "
-        resp=self.uos1.uoscall(cmd)
-        # utz.print(resp[uos.BUFOUT])
+        self.uos1.uoscall(cmd)
+        utz.print(self.uos1.bufout_txt)
 
-        respj=json.loads(resp[uos.BUFOUT])
+        respj=json.loads(self.uos1.bufout_txt)
         utz.jprint(respj["items"][0]["kind"])
+        return
+
+    def crt_namespaces(self, cls):
+        utz.enter2()
+       
+        cmd=" kubectl config use-context {cls}  -o json ".format(cls=cls)
+        self.uos1.uoscall(cmd)
+        if(self.uos1.bufout_txt !=None):
+            utz.print(self.uos1.bufout_txt)
+
+        cmd=" kubectl  create namespace av1 -o json "
+        self.uos1.uoscall(cmd)
+        if(self.uos1.bufout_txt !=None):
+            utz.print(self.uos1.bufout_txt)
+
         return
         
 
@@ -35,17 +49,14 @@ class test:
         utz.enter2()
          
         cmd=" gcloud container clusters delete {cls} -q  --format=json ".format(cls=cls)
-        resp=self.uos1.uoscall(cmd)
-        utz.print(resp[uos.BUFOUT])
+        self.uos1.uoscall(cmd)
+        utz.print(self.uos1.bufout_txt)
 
-        # respj=json.loads(resp[uos.BUFOUT])
-        # utz.jprint(respj["currentNodeCount"])
 
         cmd=" gcloud container clusters create {cls} --format=json ".format(cls=cls)
-        resp=self.uos1.uoscall(cmd)
-        # utz.print(resp[uos.BUFOUT])
+        self.uos1.uoscall(cmd)
 
-        respj=json.loads(resp[uos.BUFOUT])
+        respj=json.loads(self.uos1.bufout_txt)
         utz.print("currentNodeCount")
         utz.jprint(respj[0]["currentNodeCount"])
         return
@@ -62,7 +73,7 @@ class test:
         # nsec=2
         # utz.sleep(nsec,"sleep ithr={ithr} ".format(ithr=ithr))
         self.gcp_cluster(cls)
-        self.kpods(cls)
+        self.crt_namespaces(cls)
         utz.print("thread: end   work ithr={ithr}".format(ithr=ithr))
 
         return 
@@ -72,7 +83,7 @@ class test:
         self._lock = threading.Lock()
 
         lst_thr=list()
-        for ithr in range(2):
+        for ithr in range(1):
             # utz.sleep(1,"sleep ")
             pthr=threading.Thread(target=self.thr1,args=(ithr,))
             lst_thr.append(pthr)
@@ -89,7 +100,8 @@ def main():
     test1.thr_run()
     # cls="cls1"
     # test1.gcp_cluster(cls)
-    # test1.kpods(cls)
+    # test1.namespaces(cls)
+    # test1.crt_namespaces(cls)
 
 
 
